1aed4c6739142a814f98db3a86c6f6ec597350d0
Andrei <casterweb@tut.by>
2021-10-12 15:37:25 +0300

    interceptor

---
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthenticationHttp} from "../../services/authentication-http.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit,OnDestroy {
+
+  form: any;
+  aSub: Subscription | any;
+
+
+  constructor(private authenticationHttp:AuthenticationHttp,
+              private router: Router,
+              private route: ActivatedRoute ) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null,[Validators.required, Validators.minLength(6)])
+    })
+
+    this.route.queryParams.subscribe((params:Params) => {
+      if(params['registered']){
+        //Теперь вы можете войти в систему используя свои данные
+      } else if (params ['accessDenied']){
+        //Для начало авторизуйтесь в системе
+      }
+    })
+
+  }
+
+  ngOnDestroy(){
+    if (this.aSub){
+      this.aSub.unsubscribe()
+    }
+  }
+
+  onSubmit(){
+    this.form.disable()
+
+   this.aSub = this.authenticationHttp.login(this.form.value).subscribe(
+      ()=> this.router.navigate(['/user']),
+      error => {
+        this.form.enable()
+      }
+    )
+  }
+
+}
---
+++ src/app/pages/sign-in/sign-in.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignInRoutingModule } from './sign-in-routing.module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SignInRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class SignInModule { }
