f3325d06c85f1498509f77270f01489aef1356b5
Andrei <neatsoft.user@neatsoft.io>
2021-10-18 08:38:11 +0300

    all comments done!)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticationHttp } from './services/authentication-http.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
